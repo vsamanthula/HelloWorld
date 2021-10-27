29da1549d32078d97dc1ce1f0205df60600b3550
vsamanthula <vsamanthula@gmail.com>
2021-10-27 16:30:18 -0400

    Added Hello World program

---
+++ src/com/technet/training/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.technet.training;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
+	}
+
+}
