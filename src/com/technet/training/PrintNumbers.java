62f5139625191484b228c0e397224c0fb5b454cb
vsamanthula <vsamanthula@gmail.com>
2021-10-27 16:43:58 -0400

    added print numbers

---
+++ src/com/technet/training/PrintNumbers.java
@@ -0,0 +1,14 @@
+package com.technet.training;
+
+public class PrintNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 0;i <= 10; i++) {
+			System.out.println("Number: " + i);
+		}
+
+	}
+
+}
