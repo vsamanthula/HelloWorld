974612cf1402d1cddb107854cb51e290206e904d
vsamanthula <vsamanthula@gmail.com>
2021-10-27 17:07:42 -0400

    Added even numbers

---
+++ src/com/technet/training/EvenNumbers.java
@@ -0,0 +1,16 @@
+package com.technet.training;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 1;
+		while(i <= 10) {
+			System.out.println("Number: " + i);
+			i++;
+		}
+
+	}
+
+}
